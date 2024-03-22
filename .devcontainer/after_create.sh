39c2645530b1684abaee28063bf8c65aa319518d
Vitaliy Artemov <olivera507224@yandex.ru>
2024-03-22 23:08:17 +0000

    devcontainer: change after_create script

--- .devcontainer/after_create.sh
+++ .devcontainer/after_create.sh
@@ -4,7 +4,6 @@ npm install
 
 curl -L https://tarantool.io/knVRRof/release/3/installer.sh | bash
 sudo apt-get -y install tarantool tt
+sudo cp -f ./tt.yaml /etc/tarantool
 apt-get install -y tarantool-dev
 tt rocks install --tree ./build/.rocks http
-
-sudo cp -f ./.devcontainer/tt.yaml /etc/tarantool
